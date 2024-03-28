512312983b1efd6c406e61db1353f56250dbbbba
Djmesk <dhlhny@gmail.com>
2024-03-28 08:04:58 +0300

    minor adjustment

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosBase = axios.create({
-  // baseURL: "http://localhost:5006/api",
-  baseURL: "https://evangadi-forum-backend-api.onrender.com/api",
+  baseURL: "http://localhost:5006/api",
+  // baseURL: "https://evangadi-forum-backend-api.onrender.com/api",
 });
 export default axiosBase;
